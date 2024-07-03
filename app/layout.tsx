71467b01e4b79df4b68e0ce1bf6d49597b50ede2
hakeem-jc <hakeemclarke14@gmail.com>
2024-07-03 16:49:43 -0500

    feature/update-meta-data

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,14 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hakeem's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Hakeem Clarke - Senior Software Developer",
+  description: "Next.js / JavaScript / Full Stack Developer Portfolio",
+  keywords:
+    "Next.js, React, JavaScript, Typescript, Software Development, Developer Portfolio",
+  creator: "Hakeem Clarke",
+  robots: "index, follow",
+  icons: "https://www.hakeemclarke.com/favicon.jpg",
+  viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({
@@ -19,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.jpg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
