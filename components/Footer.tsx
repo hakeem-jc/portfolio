c71c34f6f2026f0dde7f5c5c72ec36634c0fd777
hakeem-jc <hakeemclarke14@gmail.com>
2024-07-04 22:51:13 -0500

    bugfix: Fix footer spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { socialMedia } from "@/data";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full pt-20 pb-5" id="contact">
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <Image
           src="/footer-grid.svg"
@@ -14,7 +14,7 @@ const Footer = () => {
       </div>
 
       <div className="flex md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
+        <p className="mb-6 md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Hakeem Clarke
         </p>
 
