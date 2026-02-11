e7b42094ab49e0b875149f1ee2ccaa16127760d7
hakeem-jc <hclarke.dev@gmail.com>
2026-02-11 14:46:19 -0500

    feat: Move favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
