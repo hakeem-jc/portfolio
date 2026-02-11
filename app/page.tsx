75bee69e9814ff320f534fbb52e53cb21e3c4d88
hakeem-jc <hclarke.dev@gmail.com>
2026-02-11 15:37:19 -0500

    feat: Update color scheme

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Hero from "@/components/Hero";
 import Grid from "@/components/ui/grid/Grid";
+import { navItems } from "@/data";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 bg-[url(/bg.png)] bg-no-repeat">
-      <Hero />
-      <Grid />
+    <main>
+      <div className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 bg-[url(/bg.png)] bg-no-repeat">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+      </div>
+      <Footer />
     </main>
   );
 }
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -7,14 +7,14 @@ interface SkillProps {
 
 const Skill:FC<SkillProps> = ({ name, image }) => {
     return (
-      <div className="w-40 rounded-lg shadow bg-gray-800 border-gray-700 p-5 max-h-32">
+      <div className="w-44 rounded-lg shadow bg-secondary border-gray-700 p-5 max-h-32">
         <div className="flex flex-col items-center pb-2">
           <img
-            className="w-12 h-12 mb-3 rounded-full shadow-lg"
+            className="w-14 h-14 mb-3 rounded-full shadow-lg"
             src={image}
             alt="Skill Logo"
           />
-          <h5 className="mb-1 font-medium text-gray-900 dark:text-white">
+          <h5 className="mb-1 font-medium text-white">
             {name}
           </h5>
         </div>
