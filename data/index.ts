d0c3a431996c66c70777f693d31382e73ab8b387
hakeem-jc <hclarke.dev@gmail.com>
2026-02-11 15:03:55 -0500

    feat: Add data

---
+++ data/index.ts
@@ -0,0 +1,213 @@
+export const navItems = [
+  { name: "LinkedIn", link: "https://www.linkedin.com/in/hakeemclarke/" },
+  { name: "GitHub", link: "https://github.com/hakeem-jc" },
+  { name: "Dribbble", link: "https://dribbble.com/HakeemC" },
+];
+
+export const skills = [
+  {
+    name: "React",
+    image: "/skills/react.svg",
+  },
+  {
+    name: "Next.js",
+    image: "/skills/next-js.svg",
+  },
+  {
+    name: "TypeScript",
+    image: "/skills/typescript.svg",
+  },
+  {
+    name: "JavaScript",
+    image: "/skills/javascript.svg",
+  },
+  {
+    name: "Docker",
+    image: "/skills/docker.svg",
+  },
+  {
+    name: "Express.js",
+    image: "/skills/express-js.svg",
+  },
+  {
+    name: "Node.js",
+    image: "/skills/node-js.svg",
+  },
+  {
+    name: "Jest",
+    image: "/skills/jest.svg",
+  },
+  {
+    name: "React Testing Library",
+    image: "/skills/react-testing-library.svg",
+  },
+  {
+    name: "GitHub Actions",
+    image: "/skills/github-actions.svg",
+  },
+  {
+    name: "Playwright",
+    image: "/skills/playwright.svg",
+  },
+  {
+    name: "MongoDB",
+    image: "/skills/mongodb.svg",
+  },
+  {
+    name: "SQL",
+    image: "/skills/sql.svg",
+  },
+  {
+    name: "Postgres",
+    image: "/skills/postgresql.svg",
+  },
+  {
+    name: "AWS",
+    image: "/skills/aws.svg",
+  },
+  {
+    name: "Open AI API",
+    image: "/skills/openai.svg",
+  },
+  {
+    name: "Python",
+    image: "/skills/python.svg",
+  },
+  {
+    name: "Rest API",
+    image: "/skills/rest-api.svg",
+  },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "6+ years of building industry grade web applications",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/hero-bg.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Bilingual and relocating to Spain",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I'm always learning and growing",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Fluent in English and Spanish",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a microlending app",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "GPT Light",
+    des: "Get all your questions answered seriously or whimsically",
+    img: "/projects/gpt-light.png",
+    iconLists: ["/next.svg", "/re.svg", "/ts.svg", "/oa.svg"],
+    link: "https://gpt-light.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Xoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Xoom",
+    img: "/projects/xoom.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://xoom-video-chat.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Lone",
+    des: "A full-stack loan application platform",
+    img: "/projects/lone.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://loan-calculator-tan.vercel.app/",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Senior Software Developer",
+    desc: "Designed and built various internal web apps for businesses, from initial concept to production",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 2,
+    title: "Software Developer",
+    desc: "Managed and updated a large scale banking system in React and TypeScript",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Junior Software Developer",
+    desc: "Maintained a legacy banking application in JS and Java and built a modernized version in React and Spring Boot",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/link.svg",
+    src: "https://www.linkedin.com/in/hakeemclarke/",
+  },
+  {
+    id: 2,
+    img: "/git.svg",
+    src: "https://github.com/hakeem-jc",
+  },
+  {
+    id: 3,
+    img: "/dribbble.svg",
+    src: "https://dribbble.com/HakeemC",
+  },
+];
