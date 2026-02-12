8bfd51e0e3806b946dd9828f32e487078e2c3c7d
hakeem-jc <hclarke.dev@gmail.com>
2026-02-11 22:00:11 -0500

    feat: Code cleanup and text changes

--- data/index.ts
+++ data/index.ts
@@ -92,7 +92,7 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "Bilingual and relocating to Spain",
+    title: "Relocating to Spain",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
