adf831fbf9ff3f5669ee3c9805e067635b93c49a
hakeem-jc <hclarke.dev@gmail.com>
2026-02-17 19:17:11 -0500

    temp: Comment out grid

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Hero from "@/components/Hero";
 import { navItems } from "@/data";
 import dynamic from "next/dynamic";
-import Grid from "@/components/ui/grid/Grid";
+// import Grid from "@/components/ui/grid/Grid";
 
 const GridBG = dynamic(() => import("@/components/ui/GridBG"));
 const Footer = dynamic(() => import("@/components/Footer"));
@@ -13,7 +13,7 @@ export default function Home() {
       <div className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 bg-[url(/bg.png)] bg-no-repeat">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid />
+        {/* <Grid /> */}
       </div>
 
       <GridBG />
